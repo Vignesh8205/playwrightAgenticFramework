0f204b83894f44dde3e2d2907a400e1992877bd7
Vignesh8205 <vigneshphr2002@gmail.com>
2026-08-18 23:58:35 +0530

    chore: remove test-agents from git tracking

--- vscode-qa-agent-installer/test-agents/utils/env-helpers.ts
+++
@@ -1,7 +0,0 @@
-export class EnvHelpers {
-  static getBaseUrl(): string {
-    const url = process.env.BASE_URL;
-    if (!url) throw new Error('BASE_URL environment variable is not set');
-    return url;
-  }
-}
